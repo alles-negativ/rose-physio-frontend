a65bc4f727bc9ecff30a6771a7a0a7b421b61cd7
Aurelian Ammon <aurelianammon@gmail.com>
2021-10-21 16:51:38 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,6 @@
 <script>
 export default {
   async asyncData({ params, $axios }) {
-    // This could also be an action dispatch or whatever
     const data = await $axios.$post('/api/',{
       query: "page('test')"
     }, {
