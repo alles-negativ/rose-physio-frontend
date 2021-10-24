96453867cb01824d0e7a55ce8c36c64dfca9f69c
Aurelian Ammon <aurelianammon@gmail.com>
2021-10-24 08:27:13 +0200

    dynamic slug pages

---
+++ pages/_slug.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ content }}</h1>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $axios }) {
+    const data = await $axios.$post('http://localhost:8000/api/query',{
+      query: "page('" + params.slug + "')"
+    }, {
+      auth: {
+        username: "hello@alles-negativ.ch",
+        password: "letmein123"
+      }
+    })
+    const content = data.result
+    return { 
+      content 
+    }
+  }
+}
+</script>
