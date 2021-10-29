b2665e674822329b33bb3542171c32510134fc2a
Aurelian Ammon <aurelianammon@gmail.com>
2021-10-29 15:53:53 +0200

    people

---
+++ components/People.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div v-for="person in people" :key="person.id">
+            <img :src="person.images[0].url">
+            <h3>{{ person.title }}</h3>
+            <p style="color: grey;">{{ person.introtext }}</p>
+            <p>{{ person.contenttext }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'People',
+
+    data() {
+        return {
+            people: []
+        }
+    },
+    async fetch() {
+        const { json: data } = await this.$kirby.find({
+            "query": "page('team').children",
+            "select": {
+                "title": true,
+                "introtext": true,
+                "contenttext": true,
+                "contentimage": true,
+                "images": {
+                    "query": "page.files",
+                    "select": {
+                        "name": true,
+                        "url": true
+                    }
+                }
+            }
+        }, this.$nuxt.context.app.i18n.locale)
+        this.people = data.data
+    }
+}
+</script>
--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,24 +1,14 @@
 <template>
-  <p>{{ page }}</p>
+  <div>
+    <p>{{ page }}</p>
+    <People />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Slug',
   async asyncData({ app, params, $kirby }) {
-    // const data = await $axios.$post('http://localhost:8000/api/query',{
-    //   query: "page('" + params.slug + "')"
-    // }, {
-    //   auth: {
-    //     username: "hello@alles-negativ.ch",
-    //     password: "letmein123"
-    //   }
-    // })
-    // console.log(params)
-    // const content = data.result
-    // return { 
-    //   content 
-    // }
     const { json: page } = await $kirby.find({
       "query": "page('" + params.slug + "')"
     }, app.i18n.locale)
