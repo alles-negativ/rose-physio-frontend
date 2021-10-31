6d2112f6dd9c6fe8805c6fa57d09bc302f3bbf88
Aurelian Ammon <aurelianammon@gmail.com>
2021-10-31 18:28:03 +0100

    static build

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,9 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  // create a static page
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'rose-physio',
