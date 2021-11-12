a8ed6471447de1024d4c35d0e470600745d22e04
CarloNatter <61711997+GenepoolWoW@users.noreply.github.com>
2021-11-12 13:51:53 +0100

    Articles Component

--- components/Articles/Articles.vue
+++ components/Articles/Articles.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="wrapper"> 
-        <ul class="container">
-            <li v-for="article in articles" :key="article.id" class="content">
+        <ul v-for="columns in sortedArticles" :key="columns.id" class="container">
+            <li v-for="article in columns" :key="article.id" class="content">
                 <nuxt-img :src="article.images[0].url" />
                 <h3>{{ article.title }}</h3>
                 <p class="text__big">{{ $moment(article.date).format("DD.MM.YYYY") }}</p>
                 <p>{{ article.contenttext }}</p>
             </li>
-        </ul>  
+        </ul>
     </div>
 </template>
 
@@ -17,7 +17,8 @@ export default {
 
     data() {
         return {
-            articles: []
+            articles: [],
+            numberOfColumns: 0
         }
     },
     async fetch() {
@@ -38,7 +39,43 @@ export default {
             }
         }, this.$nuxt.context.app.i18n.locale)
         this.articles = data
-    }
+    },
+
+    methods: {
+        getScreen: function(){
+            if (process.client) {
+                if (window.innerWidth < 575) {
+                    this.numberOfColumns = 1
+                }
+                else if(window.innerWidth > 1200 ) {
+                    this.numberOfColumns = 3
+                }
+                else {
+                    this.numberOfColumns = 2
+                }
+           } 
+        }
+    },
+
+    computed: {
+        sortedArticles(){
+            var output = []
+            for (var i=0; i<this.numberOfColumns; i++) {
+                output[i] = []
+                for (var j=i; j<this.articles.length; j=j+this.numberOfColumns) {
+                    output[i].push(this.articles[j]) 
+                }
+            }
+            return output
+        }
+    },
+
+    mounted() {
+        this.getScreen()
+        window.addEventListener('resize', () => {
+        this.getScreen()
+    })
+  },
 }
 </script>
 
