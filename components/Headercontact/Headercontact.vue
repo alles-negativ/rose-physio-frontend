7467c7ff4716a120db1bdaf04d19742a7c28cfd7
CarloNatter <61711997+GenepoolWoW@users.noreply.github.com>
2021-11-18 17:34:31 +0100

    Contact Header Styling

--- components/Headercontact/Headercontact.vue
+++ components/Headercontact/Headercontact.vue
@@ -9,17 +9,23 @@
         <LanguageInput />
       </div>  
     </div>
-    <div class="header__content">
-        <span v-html="title" />
-        <p class="text__big">{{ text }}</p>
-        <div class="header__content--text">
-          <p class="text" v-html="contact.headertram"/>
-          <p class="text">{{ contact.headeropening }}</p>
-        </div> 
-        <div class="header__content--contact">
-          <a class="text text__big--white" href="mailto:info@rose-physio-fake.ch?subject=Termin&body=Guten%20Tag%2C%0A%0Aich%20w%C3%BCrde%20gerne%20einen%20Termin%20abmachen.%0A%0AGruss">{{ contact.headeremail }}</a>
-          <a class="text text__big--white" href="tel:+4733378901">{{ contact.headertel }}</a>
-        </div>
+    <div class="content__wrapper">
+      <div class="header__content">
+          <span v-html="title" />
+          <p class="text__big">{{ text }}</p>
+          <div class="header__content--text">
+            <p class="text" v-html="contact.headertram"/>
+            <p class="text">{{ contact.headeropening }}</p>
+          </div> 
+          <div class="header__content--contact">
+            <div class="linkwrapper">
+              <a class="text text__big--white" href="mailto:info@rose-physio-fake.ch?subject=Termin&body=Guten%20Tag%2C%0A%0Aich%20w%C3%BCrde%20gerne%20einen%20Termin%20abmachen.%0A%0AGruss">{{ contact.headeremail }}</a>
+            </div>
+            <div class="linkwrapper">
+              <a class="text text__big--white" href="tel:+4733378901">{{ contact.headertel }}</a>
+            </div>
+          </div>
+      </div>
     </div>
       <div class="arrow" onclick="arrowScroll()">
           <img class="arrow__img" src="~/assets/images/arrow.svg" alt="">
