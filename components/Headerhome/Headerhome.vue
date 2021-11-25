816fc6b7b5bfb8a3a4e05a4882c73fb34c8ab4f3
Aurelian Ammon <aurelianammon@gmail.com>
2021-11-25 14:24:43 +0100

    Update Headerhome.vue

--- components/Headerhome/Headerhome.vue
+++ components/Headerhome/Headerhome.vue
@@ -49,10 +49,10 @@ export default {
     }
   },
   mounted() {
-    this.$Splitting()
+    // this.$Splitting()
   },
   updated() {
-    this.$Splitting()
+    // this.$Splitting()
   }
 }
 </script>
