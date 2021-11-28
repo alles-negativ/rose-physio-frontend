6d9bf1044406b283e5290ea0ddc12c99ba3c405f
Aurelian Ammon <aurelianammon@gmail.com>
2021-11-28 12:51:28 +0100

    [add:] main header animations

--- mixins/linkClickRouting.js
+++ mixins/linkClickRouting.js
@@ -50,8 +50,48 @@
         for (let i = 0; i < this.links.length; i++) {
           this.links[i].addEventListener("click", this.navigate, false);
         }
+        const animations = [
+          {name: "headline--fall", time: 600, delay: 50},
+          {name: "headline--jump", time: 800, delay: 75}
+        ]
+        const splitting_links = this.$el.querySelectorAll(".splitting__header a")
+        for (let i = 0; i < splitting_links.length; i++) {
+          splitting_links[i].classList.add("headline");
+          const animation = animations[Math.floor(Math.random() * animations.length)]
+          splitting_links[i].onmouseenter = function(){
+            this.classList.add(animation.name);
+          }
+          splitting_links[i].onmouseleave = function(){
+            setTimeout(function(){
+               this.classList.remove(animation.name);
+            }.bind(this),1000)
+          }
+          // splitting_links[i].classList.add(animations[Math.floor(Math.random() * animations.length)]);
+          splitting_links[i].setAttribute("data-splitting","")
+        }
+        this.$Splitting()
+        if (splitting_links.constructor === NodeList && splitting_links.length > 0) {
+          setTimeout(function(){
+            this.startLoop(splitting_links, animations)
+          }.bind(this), 1000)
+        }
       },
-  
+      startLoop(links, animations) {
+        const link = links[Math.floor(Math.random() * links.length)]
+        const animation = animations[Math.floor(Math.random() * animations.length)]
+        link.classList.add(animation.name);
+
+        const text = link.getAttribute('style')
+        const matches = text.matchAll(/--char-total:(\d+);/g)
+        const num_of_chars = matches.next().value[1]
+
+        setTimeout(function(){
+          link.classList.remove(animation.name)
+          setTimeout(function(){
+            this.startLoop(links, animations)
+          }.bind(this), 1000)
+        }.bind(this), 2 * (animation.time + num_of_chars * animation.delay))
+      },
       removeListeners() {
         for (let i = 0; i < this.links.length; i++) {
           this.links[i].removeEventListener("click", this.navigate, false);
